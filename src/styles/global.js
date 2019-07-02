9ba303d5851629edd6814c6b81b626e9ed0d75a0
Sean McPherson <seanmcp25@gmail.com>
2019-07-02 19:55:34 -0400

    Add font-family

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
 body {
     margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 100%;
     -webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
