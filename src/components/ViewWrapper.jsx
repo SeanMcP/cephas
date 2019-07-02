391e69d1b2fbfa4d2c80a900c7d1bc1e80c9ad21
Sean McPherson <seanmcp25@gmail.com>
2019-07-01 23:26:54 -0400

    Add view wrapper

---
+++ src/components/ViewWrapper.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import AppFooter from 'components/AppFooter'
+import { APP } from 'constants/index'
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`
+
+const Main = styled.main`
+    flex-grow: 1;
+    overflow-y: auto;
+`
+
+function ViewWrapper({ children, title }) {
+    React.useEffect(() => {
+        document.title = `${title} - ${APP.title}`
+    }, [title])
+    return (
+        <Container>
+            <Main role="main" id="main">
+                {children}
+            </Main>
+            <AppFooter />
+        </Container>
+    )
+}
+
+ViewWrapper.propTypes = {
+    title: PropTypes.string.isRequired
+}
+
+export default ViewWrapper
