6e484bb7a911750b1264f001db2e53a4803d530e
Sean McPherson <seanmcp25@gmail.com>
2019-07-06 14:56:22 -0400

    Add skip link

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import StyleProvider from 'styles/StyleProvider'
 import { SettingsProvider } from 'store/SettingsStore'
+import SkipLink from 'components/SkipLink'
 import Router from './Router'
 
 function App() {
     return (
         <SettingsProvider>
             <StyleProvider>
+                <SkipLink />
                 <Router />
             </StyleProvider>
         </SettingsProvider>
