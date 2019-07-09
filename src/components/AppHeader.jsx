c74b20aeb883954e404e80b229b6e5ba5eb26039
Sean McPherson <seanmcp25@gmail.com>
2019-07-08 23:03:57 -0400

    Use back button link

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,30 +1,15 @@
 import React from 'react'
 import { Link } from '@reach/router'
-import styled, { css } from 'styled-components'
-import _IconLink from 'components/IconLink'
+import styled from 'styled-components'
+import IconLink from 'components/IconLink'
 import APP from 'constants/app'
 import PATH from 'constants/path'
-import IconButton from './IconButton'
-
-const flexCenterCenter = css`
-    align-items: center;
-    display: flex;
-    justify-content: center;
-`
+import { flexCenterCenter } from 'styles/mixins'
 
 const Container = styled.div`
     ${flexCenterCenter}
     height: 48px;
     position: relative;
-`
-
-const BackButton = styled(IconButton)`
-    ${flexCenterCenter}
-    height: 48px;
-    left: 0;
-    position: absolute;
-    top: 0;
-    width: 48px;
 `
 
 const HomeLink = styled(Link)`
@@ -35,24 +20,19 @@ const HomeLink = styled(Link)`
     }
 `
 
-const SettingsIconLink = styled(_IconLink)`
+const SettingsIconLink = styled(IconLink)`
     position: absolute;
     right: 0;
     top: 0;
 `
 
-function AppHeader({ backTo = PATH.ROOT }) {
+function AppHeader({ backTo }) {
     const { pathname } = window.location
+    const isRoot = pathname === PATH.ROOT
     return (
         <header>
             <Container>
-                {pathname !== PATH.ROOT && (
-                    <BackButton
-                        onClick={() => window.history.go(-1)}
-                        icon="ChevronLeft"
-                        label="Back"
-                    />
-                )}
+                <AppHeaderBackButtonLink backTo={backTo} isRoot={isRoot} />
                 <HomeLink to={PATH.ROOT}>{APP.title}</HomeLink>
                 {pathname !== PATH.SETTINGS && (
                     <SettingsIconLink
